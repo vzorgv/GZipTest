ff2c2ba8bb1f9eed59dffb162638fc501a7e5a28
Dmitry Pereverzev <vzorgv@gmail.com>
2021-03-18 22:29:58 +0300

    Stream extension

---
+++ src/Archiver/StreamExtension.cs
@@ -0,0 +1,12 @@
+﻿namespace GZipTest
+{
+    using System.IO;
+
+    internal static class StreamExtension
+    {
+        public static StreamEnumerator GetWindowEnumerator(this Stream _stream, IStreamWindowMovingStrategy movingStrategy)
+        {
+            return new StreamEnumerator(movingStrategy);
+        }
+    }
+}
