b89b6376fa2bc75b6da3b8ae318826da3bc642d1
Dmitry Pereverzev <vzorgv@gmail.com>
2021-03-18 20:57:29 +0300

    Blank project

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GZipTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
