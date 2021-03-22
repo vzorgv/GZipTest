825e22ddff6abe21b0dff731aa31999f4b2f6c3f
Dmitry Pereverzev <vzorgv@gmail.com>
2021-03-22 22:31:10 +0300

    Rename

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -14,7 +14,6 @@ namespace Tests
         public void Test1()
         {
             var archiver = new BlockArchiver();
-            archiver.Compress(@"C:\temp\_1.txt", @"C:\Temp\xxx.gzt");
         }
     }
 }
